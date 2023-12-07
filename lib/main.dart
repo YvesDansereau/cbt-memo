11118a2182b096901ab1e9985f2e5c4fa08bbec4
Yuichiro DAN <yuichirodan1022@gmail.com>
2023-12-07 15:01:41 +0900

    install home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:cbt_memo/home_page.dart';
 import 'package:flutter/material.dart';
-
-import 'stress_diary.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,29 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CBT',
+      title: 'CBT Memo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'CBT'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const StressDiaryPage(),
+      home: const HomePage(),
     );
   }
 }
